e33aa6f78fc8bc02ac10ed92801301c581d55a08
Adoc7 <jgoscicki@yahoo.fr>
2019-12-30 10:36:08 +0100

    v1

---
+++ js/code.js
@@ -0,0 +1,166 @@
+
+
+// Text en arc
+    $('.phrase').arctext({ radius: 800 });
+
+// PArallax
+
+    var parallaxBox = $('#parallax-container');
+    var strength = 0.2,
+        isMobile = false;
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        isMobile = true;
+    }
+
+    parallaxMove = function (parallaxContainer, x, y, boxWidth, boxHeight){
+        $(parallaxContainer).find('.parallax-layer').each(function () {
+            var depth = $(this).data('depth');
+            var moveX = ((boxWidth / 2) - x) * (strength * depth);
+            var moveY = ((boxHeight / 2) - y) * (strength * depth);
+
+            $(this).css({ transform: "translate3d(" + moveX + "px, " + moveY + "px, 0)" });
+            $(this).removeClass('is-out');
+        });
+    }
+
+   var resetParallaxPosition = function(parallaxContainer){
+        $(parallaxContainer).find('.parallax-layer').each(function () {
+            $(this).css({ transform: "translate3d( 0, 0, 0 )" });
+            $(this).addClass('is-out');
+        });
+        event.stopPropagation();
+    }
+
+    if (!isMobile) {
+
+        parallaxBox.mousemove(function (event) {
+            event.stopPropagation();
+            event = event || window.event;
+            var x = Math.floor(event.clientX - $(this).offset().left),
+                y = Math.floor(event.clientY - $(this).offset().top),
+                boxWidth = $(this).width(),
+                boxHeight = $(this).height();
+
+            parallaxMove(this, x, y, boxWidth, boxHeight);
+
+        });
+
+        parallaxBox.mouseleave(function (event) {
+            if (!$(event.target).is($(this))) {
+                resetParallaxPosition(this);
+            }
+        });
+
+    } else if (isMobile) {
+        var elem = $("view3d");
+
+        window.addEventListener("deviceorientation", function (event) {
+            event.stopPropagation();
+            event = event || window.event;
+
+            var rotatedY = Math.min(Math.max(parseInt(Math.floor(event.gamma)), -45), 45),
+                rotatedX = Math.min(Math.max(parseInt(Math.floor(event.beta)), -45), 45),
+                boxWidth = parallaxBox.width(),
+                boxHeight = parallaxBox.height();
+
+            var moveX = ((boxWidth / 2) * rotatedY) / 45;
+            var moveY = ((boxWidth / 2) * rotatedX) / 45;
+
+            parallaxMove(parallaxBox, moveX, moveY, boxWidth, boxHeight);
+
+        });
+    }
+
+
+         $(document).ready(function(){
+           
+
+          
+           
+      
+             /// Déplacement dans la page au clic
+
+            $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+               var page = $(this).attr('href'); // Page cible
+               var speed = 750; // Durée de l'animation (en ms)
+               var speed = 750; // Durée de l'animation (en ms)
+               $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+               return false;
+            });
+
+
+  
+
+
+            // MODAL
+            $('.modal').each(function(){
+                var src = $(this).find('iframe').attr('src');
+                $(this).on('click', function(){
+                $(this).find('iframe').attr('src', '');
+                $(this).find('iframe').attr('src', src);
+                 });
+                });
+
+            /// Rendre visible au clic
+            // Lire plus
+            var temps_montre = 2000;
+
+      
+      
+              afficher_div_vente_masque = function(){
+                $("#btve").hide()
+                  $("#vente").slideDown().show(temps_montre)
+                 
+               }
+               afficher_div_reserv_masque = function(){
+                $("#btre").hide()
+                  $("#reserv").slideDown().show(temps_montre)
+                
+               }
+               //toggle
+               var timer=3000;
+               
+          
+
+            // AUTRES TOGGLES
+             $(".but").hover(function() {
+                    $("." + $(this).data('target')).slideToggle(timer);
+                
+                });
+                            
+                $(".butpdf").click(function() {
+                    $("." + $(this).data('target')).slideToggle(timer);
+                
+                });
+
+              
+
+            
+
+              $(".suite").hover(function() {
+                $("." + $(this).data('target')).show();
+                $(this).hide();
+              });
+
+
+
+/// Disparition du menu après clic
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggler:visible').click();
+    });
+    $('.dropdown-menu  a').click(function(){
+      $('.dropdown-menu:visible').click();
+      $('.navbar-toggler:visible').click();
+    });
+
+
+
+
+
+});
+
+  
+
+// Loader
+
